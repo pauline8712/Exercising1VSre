ad2ad23d5c0b4205d9a151c978eca8edcc491d4d
pauline8712 <plinekaeorg@gmail.com>
2025-09-05 09:00:28 +0200

    wrote in the file

--- Exercising1VS/Intro.cs
+++ Exercising1VS/Intro.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exercising1VS
-{
-    internal class Intro
-    {
-    }
-}
+﻿Hello, My name is Pauline and I am 21 years old
+I want to learn more about C# and Github. It starts to get interesting but its a little bit hard.
+I like to lift weights, read books and socialize.
+Goodbye!!
