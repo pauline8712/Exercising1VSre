aaf2a1c7779d7a457aa475f4783fb92fdfb22223
pauline8712 <plinekaeorg@gmail.com>
2025-09-05 12:30:54 +0200

    Theres now second files with 5 points what i look forward to in this program

---
+++ Exercising1VS/Secondintro.cs
@@ -0,0 +1,18 @@
+﻿# C - sharp
+- Jag gillar att C# är lite större på ett sätt som gör att många programmerare kan relatera till språket. Att det är brett.
+
+# GitHub
+- Jag vill lära mig GitHub eftersom det är många som känner sig intimidated av det och jag vill inte känna så. Vill vara självsäker i det så jag kan fortsätta lära mig.
+
+# Nätverka
+- Jag vill skapa min egen nätverk med kontakter som jag kan lära mig av och att jag får vara deras kontakter också. Att få hjälp att fortsätta framåt
+
+# Team
+- Vill vara med i en bra grupp som har ambition och passion för att lära sig mer. Balanserar studier att återhämta sig och att ha disciplinen. Gör allt i skolan och göra sin del i projektet. Bygga relationer.
+
+# Event och mässor
+- Det kommer vara så spännande med event att få lära sig det andra ha fått lära sig och att de vill ge vidare av sin kunskap.
+- Mässor där jag kan gå runt och mingla. Ställa frågor och vara mer nyfiken
+
+# Sista dagen
+- Jag längtar att få se hur långt jag har kommit och hur mycket kunskap jag har fått med tiden. Att bara få reflektera och vara stolt över allt. Att kunna ge vidare.
